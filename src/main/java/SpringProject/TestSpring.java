24ae48b21812d3f34b50614ab80564a7363aeb24
dianazavr1 <159002661+dianazavr1@users.noreply.github.com>
2024-09-28 00:24:40 +0600

    singleton, prototype

--- src/main/java/SpringProject/TestSpring.java
+++ src/main/java/SpringProject/TestSpring.java
@@ -2,6 +2,8 @@ package SpringProject;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.sql.SQLOutput;
+
 public class TestSpring {
     public static void main (String[] args){
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
@@ -9,10 +11,25 @@ public class TestSpring {
         );
       //  Music music = context.getBean("musicBean", Music.class);
         //  MusicPlayer musicPlayer = new MusicPlayer(music);
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+
+
+        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+
+        System.out.println(comparison);
+
+        System.out.println(firstMusicPlayer);
+        System.out.println(secondMusicPlayer);
+
+        firstMusicPlayer.setVolume(10);
+
+        System.out.println(firstMusicPlayer.getVolume());
+        System.out.println(secondMusicPlayer.getVolume());
+
+//        musicPlayer.playMusic();
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
         context.close();
     }
 
